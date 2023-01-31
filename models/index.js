01cd8fe95779a118defef43338437e0acc461d56
lileee <112982735+lileeeeeee@users.noreply.github.com>
2023-01-31 11:14:16 -0600

    set up models and attributes, attempted to seed the database, getting error on product tag.

---
+++ models/index.js
@@ -0,0 +1,33 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+// N:M
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'tag_id' });
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'product_id' });
+
+// Tag.belongsToMany(Product,{
+//   foreignKey: 'product_id',
+// });
+// Product.hasMany(Tag, {
+//   foreignKey: 'product_id',
+// });
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
