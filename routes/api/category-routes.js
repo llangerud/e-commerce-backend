ea0825cc9a0e12feeb292a18fc88f9d5a13078fc
lileee <112982735+lileeeeeee@users.noreply.github.com>
2023-01-31 22:44:05 -0600

    updated category put route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,7 +37,7 @@ router.post('/',async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try{
-    await Category.update({ category_name: "umbrella"}, {
+    await Category.update(req.body, {
       where: {
         id: req.params.id
       }
